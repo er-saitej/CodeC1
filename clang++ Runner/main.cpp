84695c67ea9e999909a03280b07dca0f8fc8a3fc
saitejsunkara9 <saitejsunkara9@gmail.com>
2021-12-07 12:44:50 +0530

    Snippet Created

--- clang++ Runner/main.cpp
+++ clang++ Runner/main.cpp
@@ -1,14 +1,13 @@
 #include<bits/stdc++.h>
-#define PERSONAL
 using namespace std;
 
 int main() {
-    #ifdef PERSONAL
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
     #endif
-    int n;
-    cin>>n;
-    cout<<n;
+
+
+
     return 0;
 }
