f5931c7c462b23793baae5212f4f7e88542ee502
saitejsunkara9 <saitejsunkara9@gmail.com>
2021-12-07 13:54:50 +0530

    Test cases pilot updated

--- clang++ Runner/main.cpp
+++ clang++ Runner/main.cpp
@@ -3,13 +3,25 @@
 #define int long long int
 using namespace std;
 
+void solve() {
+
+}
+
 int32_t main() {
+
+    clock_t initialTime = clock();
+
     #ifndef ONLINE_JUDGE
         freopen("input.txt", "r", stdin);
         freopen("output.txt", "w", stdout);
+        freopen("error.txt", "w", stderr);
     #endif
 
-    
+    int tests = 1;
+    cin>>tests;
+    while(tests--) solve();
 
+    cerr<<((double)clock() - initialTime)/CLOCKS_PER_SEC;
+    
     return 0;
 }
