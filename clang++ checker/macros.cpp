cf8f5d948ae38446da37d9dc648c2bf28fd899a6
saitejsunkara9 <saitejsunkara9@gmail.com>
2021-12-07 13:00:43 +0530

    Macros

---
+++ clang++ checker/macros.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    ll n = 1e18; // (1*10^18)
+    ll m = 9e18; // (9*10^18) // maximum number that can be printed in cpp
+    cout<<n<<'\n';
+    cout<<m;
+    return 0;
+}
